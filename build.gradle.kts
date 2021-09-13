c9c7ee84f669f29dfee05cd73bc28663ca01ceeb
Viktor Likhanov <volixanov@unitbean.io>
2021-09-13 11:43:01 +0300

    - refactor: updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.1")
-        classpath(kotlin("gradle-plugin", version = "1.5.21"))
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath(kotlin("gradle-plugin", version = "1.5.30"))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.17.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
